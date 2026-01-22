0cc83e5dd3bd49ecb1493ee190fb0f6c5018a3ea
hazkmedia <vantanpro1st@gmail.com>
2026-01-22 08:28:55 +0700

    lv

--- api/index.js
+++ api/index.js
@@ -1,31 +1,38 @@
-module.exports = (req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/html; charset=utf-8"
-  });
+export default function handler(req, res) {
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
 
-  res.end(`<!DOCTYPE html>
-<html lang="vi">
+    res.status(200).send(`<!DOCTYPE html>
+<html lang="en">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <title>Careers at Louis Vuitton</title>
+
 <style>
-html, body {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  overflow: hidden;
-}
-iframe {
-  width: 100%;
-  height: 100vh;
-  border: none;
-  display: block;
-}
+  html, body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    overflow: hidden;
+  }
+
+  iframe {
+    width: 100%;
+    height: 100vh;
+    border: none;
+    display: block;
+  }
 </style>
 </head>
+
 <body>
-<iframe src="https://blackdognewmedia.co.uk" loading="lazy" allowfullscreen></iframe>
+
+<iframe
+  src="https://blackdognewmedia.co.uk"
+  loading="lazy"
+  allowfullscreen>
+</iframe>
 </body>
 </html>`);
-};
+}
